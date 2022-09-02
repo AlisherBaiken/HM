1b7cafa9793081744f7befe89fc66e2426a751e6
Alisher <alisherbaiken@gmail.com>
2022-09-02 21:45:32 +0600

    homework01

---
+++ hm_01/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2){
+    Console.Write($"{number1} больше чем {number2}");
+}else {
+    Console.Write($"{number2} больше чем {number1}");
+}
+
+Console.WriteLine("Введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number_3 = int.Parse(Console.ReadLine());
+int max = 0;
+ 
+if () 
