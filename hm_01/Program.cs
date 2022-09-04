ef7d75b71e7558d7539dc8c651d211bd184bf336
Alisher <alisherbaiken@gmail.com>
2022-09-04 15:34:40 +0600

    hm01

--- hm_01/Program.cs
+++ hm_01/Program.cs
@@ -5,17 +5,23 @@ Console.WriteLine("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine());
 
 if (number1 > number2){
-    Console.Write($"{number1} больше чем {number2}");
-}else {
-    Console.Write($"{number2} больше чем {number1}");
+    Console.WriteLine($"{number1} больше чем {number2}");
+}
+else if(number1 < number2) {
+    Console.WriteLine($"{number2} больше чем {number1}");
+} else {
+    Console.WriteLine("Ввели два одинаковые числа");
 }
 
-Console.WriteLine("Введите первое число: ");
-int number_1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int number_2 = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите третье число: ");
-int number_3 = int.Parse(Console.ReadLine());
-int max = 0;
+// Console.WriteLine("Введите первое число: ");
+// int number_1 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int number_2 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int number_3 = int.Parse(Console.ReadLine());
+// int max = 0;
  
-if () 
+
+
+
+
