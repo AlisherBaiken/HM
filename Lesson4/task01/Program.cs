ff704c3f9b2aeb9cc79281a90dcdac016a26b93d
Alisher <alisherbaiken@gmail.com>
2022-09-17 16:07:57 +0600

    Prakktika 4 C#

---
+++ Lesson4/task01/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+Console.Clear();
+Console.Write("Введите числе: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел = {GetSumNums(num)}");
+int GetSumNums(int numbers)
+{
+    int sum = 0;
+    while (numbers > 0)
+    {
+        sum += numbers; // sum = sum + numbers
+        numbers--;     // numbers = numbers -1 
+    }
+    return sum;
+}
