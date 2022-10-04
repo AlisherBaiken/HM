c2b31a6b778b7922aba4ff9e55805372b0f366fb
Alisher <alisherbaiken@gmail.com>
2022-10-04 20:04:25 +0600

    hm07

---
+++ hm_07/Program.cs
@@ -0,0 +1,100 @@
+﻿//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+// Console.Write("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine());
+
+// double[,] array = GetArray(rows, columns, 0, 9);
+// PrintArray(array);
+// Console.WriteLine();
+// PrintArray(array);
+
+// double[,] GetArray(int m, int n, int minValue, int maxValue)
+// {
+//     double[,] result = new double[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = Math.Round(new Random().Next(minValue, maxValue) + new Random().NextDouble(), 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(double[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+
+// }
+/////////////////////////////////////////////////////////////////////
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// [1, 7] -> такого числа в массиве нет
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 9);
+PrintArray(array);
+Console.WriteLine();
+FindArray(array);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void FindArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[i,j]== inArray[i,j]);
+            { 
+                Console.Write($"{inArray[i, j]} "); 
+            }
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
