24a12882e45b3b0bb1216303adb45c098aad6a1e
Alisher <alisherbaiken@gmail.com>
2022-10-08 14:12:12 +0600

    lesson08

---
+++ Lesson_8/task01/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+int[,] array = GetArray(5, 5, 0, 9);
+PrintArray(array);
+int[,] SwappedArray = SwapRows(array);
+Console.WriteLine();
+PrintArray(SwappedArray);
+int[,] SwapRows(int[,] arrayToSwap)
+{
+    int rows = arrayToSwap.GetLength(0);
+    int columns = arrayToSwap.GetLength(1);
+    int [] rowsTemp = new int [columns];
+    for (int i = 0; i < columns; i++)
+    {
+        rowsTemp[i]=arrayToSwap[0,i];
+        arrayToSwap[0,i] = arrayToSwap[rows-1,i];
+        arrayToSwap[rows-1,i]= rowsTemp[i];
+    }
+    return arrayToSwap;
+
+}
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
---
+++ Lesson_8/task02/Program.cs
@@ -0,0 +1,50 @@
+﻿//55 задача меняет строки на столбцы либо выдает ошибку 
+
+
+ int[,] array = GetArray(2, 5, 0, 9);
+PrintArray(array);
+int[,] transArray = TransposeArray(array);
+Console.WriteLine();
+PrintArray(transArray);
+
+int[,] TransposeArray(int[,] arrayToTranspose)
+{
+int rows = arrayToTranspose.GetLength(0);
+int columns = arrayToTranspose.GetLength(1);
+int[,] transposedArray = new int[columns, rows];
+for (int i = 0; i < rows; i++)
+{
+for (int j = 0; j < columns; j++)
+{
+transposedArray[j, i] = arrayToTranspose[i, j];
+}
+}
+//Console.WriteLine(String.Join(", ", rowsTemp));
+return transposedArray;
+}
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
